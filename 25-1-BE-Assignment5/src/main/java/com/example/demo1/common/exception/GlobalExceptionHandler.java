59d374f8f69a16d58458bb61cf3131d7afb5d32e
l-wanderer01 <jhjjang0423@naver.com>
2025-04-06 21:55:55 +0900

    [fix] 클라이언트 친화적인 return 형식으로 GlobalExceptionHandler 수정

--- 25-1-BE-Assignment5/src/main/java/com/example/demo1/common/exception/GlobalExceptionHandler.java
+++ 25-1-BE-Assignment5/src/main/java/com/example/demo1/common/exception/GlobalExceptionHandler.java
@@ -9,6 +9,11 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @Slf4j // 로깅을 하기 위한 어노테이션 -> 로그가 더 보기 좋게 나옴
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -32,17 +37,38 @@ public class GlobalExceptionHandler {
     }
 
     // ValidException을 처리하는 handler
-    @ExceptionHandler(MethodArgumentNotValidException.class) // 유효성 검사에 실패한 에러 처리
-    public ResponseEntity<ApiRes<?>> handerMethodArgumentNotValidException(
-            final MethodArgumentNotValidException ex
-    ) {
-        log.error(ex.getMessage()); // 디버깅을 위한 로그 찍기
+//    @ExceptionHandler(MethodArgumentNotValidException.class) // 유효성 검사에 실패한 에러 처리
+//    public ResponseEntity<ApiRes<?>> handerMethodArgumentNotValidException(
+//            final MethodArgumentNotValidException ex
+//    ) {
+//        log.error(ex.getMessage()); // 디버깅을 위한 로그 찍기
+//        return new ResponseEntity<>(
+//                ApiRes.fail(
+//                        CommonErrorType.VALIDATION_ERROR,
+//                        HttpStatus.BAD_REQUEST,
+//                        ex.getMessage()
+//                ),
+//                HttpStatus.BAD_REQUEST
+//        );
+//    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiRes<?>> handleMethodArgumentNotValidException(
+            final MethodArgumentNotValidException ex) {
+
+        // 클라이언트가 알기 쉽도록 List<Map<String, String>> 형태로 가공
+        List<Map<String, String>> errors = ex.getBindingResult() // MethodArgumentNotValidException 안의 검증 결과 객체(BindingResult를 꺼냄)
+                .getFieldErrors() // 필드 단위로 발생한 에러들을 리스트로 반환
+                .stream() // 데이터 가공
+                .map(error -> {
+                    Map<String, String> map = new HashMap<>(); // error 하나 당 하나의 map 생성
+                    map.put("field", error.getField()); // 오류가 발생한 필드명
+                    map.put("message", error.getDefaultMessage()); // 유효성 검증 실패 시 붙인 메시지 값을 넣음
+                    return map; // stream 내부에서 하나의 Map을 결과값으로 반환
+                })
+                .collect(Collectors.toList()); // map 들을 리스트로 모음
+
         return new ResponseEntity<>(
-                ApiRes.fail(
-                        CommonErrorType.VALIDATION_ERROR,
-                        HttpStatus.BAD_REQUEST,
-                        ex.getMessage()
-                ),
+                ApiRes.fail(CommonErrorType.VALIDATION_ERROR, HttpStatus.BAD_REQUEST, errors),
                 HttpStatus.BAD_REQUEST
         );
     }
